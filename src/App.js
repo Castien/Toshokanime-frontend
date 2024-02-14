3c53c5cd6b1395da2f2a65d8600e147ed081736b
Eileen Lowers <funmaster22@hotmail.com>
2024-02-13 21:57:50 -0500

    JWT token auth

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ function App() {
   return (
     <div className="App">
       <h1>Testing your durability...</h1>
+      <form action="/api/login" method="post">
+      <input name="username" placeholder="Username" />
+      <input name="password" type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+    <p>Don't have an account? <a href="signup.html">Sign up here!</a></p>
     </div>
   );
 }
